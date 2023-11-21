18e09f481b1f0151f169e4d73d3c8c7536d19073
Andre Luiz <andre.lmp@protonmail.com>
2023-11-21 17:25:04 -0300

    chore: added CORS permissions

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // Cors
+
+  app.enableCors({ origin: 'https://user-crud-fcwm.onrender.com' });
 }
 bootstrap();
