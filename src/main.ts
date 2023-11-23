a467e0710a71fba5530622374b950e7ad00e54c9
Andre Luiz <andre.lmp@protonmail.com>
2023-11-23 08:34:09 -0300

    fix: CORS policy

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // Cors
-  app.enableCors({
-    origin: 'https://user-crud-fcwm.onrender.com',
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
   });
   await app.listen(3000);
 }
